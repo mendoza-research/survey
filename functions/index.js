1ec38542a2cc383dedecc4a8ade3dd460f4902fb
Ye Joo Park <subwaymatch@gmail.com>
2019-08-19 06:49:49 +0900

    Add an API endpoint to get all responses as JSON

--- functions/index.js
+++ functions/index.js
@@ -3,6 +3,7 @@ const cors = require("cors")({ origin: true });
 const admin = require("firebase-admin");
 const { Parser } = require("json2csv");
 const fields = require("./fields.json");
+const objectPath = require("object-path");
 
 var serviceAccount = require("./firebase-admin-sdk.json");
 
@@ -11,66 +12,72 @@ admin.initializeApp({
   databaseURL: "https://kimendoz-survey.firebaseio.com"
 });
 
-function getAllResponsesAsCSV(callback) {
+async function getAllResponsesAsJSON() {
   let responsesRef = admin.firestore().collection("responses");
-  let allResponses = responsesRef
-    .get()
-    .then(snapshot => {
-      const allDocs = [];
+  let snapshot = await responsesRef.get();
 
-      snapshot.forEach(doc => {
-        allDocs.push(Object.assign({}, { sessionId: doc.id }, doc.data()));
-      });
+  const allDocs = [];
 
-      // Pre-process firestore documents
-      // Convert arrays to objects with number keys for json2csv
-      let processedDocs = allDocs.map(doc => {
-        if (doc.pages.hasOwnProperty("task2-quiz")) {
-          doc.pages["task2-quiz"].data = {
-            ...doc.pages["task2-quiz"].data
-          };
-        }
+  snapshot.forEach(doc => {
+    allDocs.push(Object.assign({}, { sessionId: doc.id }, doc.data()));
+  });
 
-        if (doc.pages.hasOwnProperty("task2-word-recall-questions")) {
-          doc.pages["task2-word-recall-questions"].data = {
-            ...doc.pages["task2-word-recall-questions"].data
-          };
-        }
-
-        return doc;
-      });
-
-      const parseOptions = {
-        fields
+  // Pre-process firestore documents
+  // Convert arrays to objects with number keys for json2csv
+  let processedDocs = allDocs.map(doc => {
+    if (doc.pages.hasOwnProperty("task2-quiz")) {
+      doc.pages["task2-quiz"].data = {
+        ...doc.pages["task2-quiz"].data
       };
+    }
 
-      const parser = new Parser(parseOptions);
-      const csv = parser.parse(processedDocs);
+    if (doc.pages.hasOwnProperty("task2-word-recall-questions")) {
+      doc.pages["task2-word-recall-questions"].data = {
+        ...doc.pages["task2-word-recall-questions"].data
+      };
+    }
 
-      return callback(csv);
-    })
-    .catch(err => {
-      console.log("Error getting documents", err);
-    });
+    return doc;
+  });
+
+  return processedDocs;
+}
+
+async function getAllResponsesAsCSV() {
+  const processedDocs = await getAllResponsesAsJSON();
+
+  const parser = new Parser(parseOptions);
+  const csv = parser.parse(processedDocs);
+
+  return csv;
 }
 
 exports.getAllResponses = functions.https.onRequest((req, res) => {
-  return cors(req, res, () => {
-    getAllResponsesAsCSV(csv => {
-      res.json({
-        data: csv
+  return cors(req, res, async () => {
+    const processedDocs = await getAllResponsesAsJSON();
+    const flattenedDocs = [];
+
+    processedDocs.forEach(doc => {
+      let flattenedDoc = {};
+
+      fields.forEach(field => {
+        flattenedDoc[field.label] = objectPath.get(doc, field.value);
       });
+
+      flattenedDocs.push(flattenedDoc);
     });
+
+    res.json({ data: flattenedDocs });
   });
 });
 
 exports.exportAllResponses = functions.https.onRequest((req, res) => {
-  return cors(req, res, () => {
-    getAllResponsesAsCSV(csv => {
-      res.setHeader("Content-disposition", "attachment; filename=export.csv");
-      res.set("Content-Type", "text/csv");
+  return cors(req, res, async () => {
+    const csv = await getAllResponsesAsCSV();
 
-      return res.status(200).send(csv);
-    });
+    res.setHeader("Content-disposition", "attachment; filename=export.csv");
+    res.set("Content-Type", "text/csv");
+
+    return res.status(200).send(csv);
   });
 });
